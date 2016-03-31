89457df5899ccf2b9c8155817d2235180d3c0e04
Aje <abcd@abcds-mac-mini.local>
2016-03-31 14:24:50 +0530

    Initial Commit

---
+++ mca/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mca
+//
+//  Created by Ajay on 31/03/16.
+//  Copyright © 2016 Ajay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
